d0e1c43b18cdae6d744f5432329f1fbf408e4cc4
Dejan Ristic <dejanr77@yahoo.co>
2019-06-21 00:45:29 +0200

    added context path

--- web/src/main/java/com/dejanristic/blog/BlogApplication.java
+++ web/src/main/java/com/dejanristic/blog/BlogApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlogApplication {
 
     public static void main(String[] args) {
+        System.setProperty("server.servlet.context-path", "/blog");
         SpringApplication.run(BlogApplication.class, args);
     }
 
