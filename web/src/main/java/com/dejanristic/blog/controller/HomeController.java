4493dfa48b01ec4e44489a66e3cea2bd4a0980db
Dejan Ristic <dejanr77@yahoo.co>
2019-06-21 02:03:20 +0200

    Added a Page not Found (404)

--- web/src/main/java/com/dejanristic/blog/controller/HomeController.java
+++ web/src/main/java/com/dejanristic/blog/controller/HomeController.java
@@ -4,6 +4,7 @@ import com.dejanristic.blog.util.UrlMappings;
 import com.dejanristic.blog.util.ViewNames;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
@@ -16,5 +17,10 @@ public class HomeController {
     @GetMapping(UrlMappings.HOME)
     public String home() {
         return ViewNames.HOME;
+    }
+
+    @RequestMapping("*")
+    public String notFoundPage() {
+        return "error/404";
     }
 }
